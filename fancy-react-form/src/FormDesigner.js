e415dc018a02080cc82ac447861be4d5c2370273
meq <fewell@gmail.com>
2018-07-04 15:51:54 -0400

    Added blank components for the Form Designer

---
+++ fancy-react-form/src/FormDesigner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class FormDesigner extends Component {
+  render() {
+    return (
+      <div className="FormDesigner">
+
+      </div>
+    );
+  }
+}
+
+export default FormDesigner;
